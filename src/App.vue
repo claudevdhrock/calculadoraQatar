1d6bf456757b1572b0b510b866fb931c1a4acb85
Claudio <66223125+claudevdhrock@users.noreply.github.com>
2023-01-16 22:19:19 -0300

    4to commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template class="StyloFather">  
+  <div class="StyloChilds">
+    <PartidosJudados/>
+  </div>
+</template>
+
+<script>
+import PartidosJudados from './components/PartidosJudados.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    PartidosJudados,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.StyloChilds{
+  display: flex;
+  justify-content: space-evenly;
+}
+@media only screen and (max-width: 1400px) {
+  .StyloChilds {
+    display: grid;
+    justify-items: center;
+  }
+}
+
+.StyloTabla{
+  display: flex;
+}
+
+
+.StyloFather{
+  display: flex;
+}
+
+</style>
